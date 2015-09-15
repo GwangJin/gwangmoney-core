aaac663092e4cdcd878bb64c054cb4365d345409
yezune <yezune@gmail.com>
2015-09-15 11:42:44 +0900

    발행량 조

--- src/main.h
+++ src/main.h
@@ -98,9 +98,9 @@ static const unsigned int DATABASE_WRITE_INTERVAL = 3600;
 static const unsigned int MAX_REJECT_MESSAGE_LENGTH = 111;
 
 /** FairPay: Dust Threshold: outputs below this value in satoshis are assessed an additional 1000 bytes per txout */
-static const CAmount DUST_THRESHOLD = 100000; // 0.001 COIN
+static const CAmount DUST_THRESHOLD = 0; // 0.000 COIN
 /** FairPay: Default TX Fee per 1000 bytes */
-static const CAmount DEFAULT_TX_FEE = 100000; // 0.001 COIN
+static const CAmount DEFAULT_TX_FEE = 0; // 0.000 COIN
 
 /** FairPay: default minimum input threshold, override with -mininput */
 static const CAmount DEFAULT_MINIMUM_INPUT_THRESHOLD = DUST_THRESHOLD / 100; // 0.00001 COIN
